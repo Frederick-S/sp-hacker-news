981966b7fe66b08fa18c9090cf3b4b27b96ad888
Frederick-S <mao_xiaodan@hotmail.com>
2020-03-25 11:40:01 +0800

    feature: render news list

--- src/webparts/hackerNews/components/HackerNews.tsx
+++ src/webparts/hackerNews/components/HackerNews.tsx
@@ -1,8 +1,11 @@
 import * as React from 'react'
 import { IHackerNewsProps } from './IHackerNewsProps'
 import { Label } from 'office-ui-fabric-react/lib/Label'
+import { List } from 'office-ui-fabric-react/lib/List'
 import { Pivot, PivotItem, IPivotStyles } from 'office-ui-fabric-react/lib/Pivot'
 import { IStyleSet } from 'office-ui-fabric-react/lib/Styling'
+import axios from 'axios'
+import FeedItem from '../data/FeedItem'
 
 const pivotStyles: Partial<IStyleSet<IPivotStyles>> = {
   root: {
@@ -10,20 +13,34 @@ const pivotStyles: Partial<IStyleSet<IPivotStyles>> = {
   }
 }
 
-export default class HackerNews extends React.Component<IHackerNewsProps, {}> {
+interface IState {
+  news: FeedItem[]
+}
+
+export default class HackerNews extends React.Component<IHackerNewsProps, IState> {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      news: []
+    }
+  }
+
   public render(): React.ReactElement<IHackerNewsProps> {
+    const { news } = this.state
+
     return (
       <Pivot aria-label="Hacker News" styles={pivotStyles}>
         <PivotItem
-          headerText="Top"
+          headerText="News"
           headerButtonProps={{
             'data-order': 1
           }}
         >
-          <Label>Pivot #1</Label>
+          <List items={news} onRenderCell={this.onRenderCell}></List>
         </PivotItem>
         <PivotItem
-          headerText="New"
+          headerText="Newest"
           headerButtonProps={{
             'data-order': 2
           }}
@@ -31,6 +48,26 @@ export default class HackerNews extends React.Component<IHackerNewsProps, {}> {
           <Label>Pivot #2</Label>
         </PivotItem>
       </Pivot>
+    )
+  }
+
+  componentDidMount() {
+    axios.get('https://api.hnpwa.com/v0/news/1.json')
+      .then(response => {
+        this.setState({
+          news: response.data
+        })
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  }
+
+  private onRenderCell(item: FeedItem, index: number | undefined): JSX.Element {
+    return (
+      <div>
+        <div>{item.title}</div>
+      </div>
     )
   }
 }
---
+++ src/webparts/hackerNews/data/FeedItem.ts
@@ -0,0 +1,12 @@
+export default interface FeedItem {
+  id: number
+  title: string
+  points?: number | null
+  user?: string | null
+  time: number
+  time_ago: string
+  comments_count: number
+  type: string
+  url?: string
+  domain?: string
+}
