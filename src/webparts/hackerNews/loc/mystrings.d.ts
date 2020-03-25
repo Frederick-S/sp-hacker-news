837ee5886fbef9b3d229c8b0cbd359e38e1f06c5
Frederick-S <mao_xiaodan@hotmail.com>
2020-03-25 10:05:44 +0800

    init: generate project

---
+++ src/webparts/hackerNews/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IHackerNewsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'HackerNewsWebPartStrings' {
+  const strings: IHackerNewsWebPartStrings;
+  export = strings;
+}
